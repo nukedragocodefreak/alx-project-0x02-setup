4d878f362f97fcea3312c1742af9e7703a5801b6
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-28 00:23:27 +0200

    add videos

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -16,3 +16,13 @@ export interface PostProps {
   content: string;
   userId: number;
 }
+export interface UserProps {
+  id: number;
+  name: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  }
