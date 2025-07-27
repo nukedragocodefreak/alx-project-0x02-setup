afb45fe495204a83f2e319e26d5e5351cab9a208
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-28 00:35:37 +0200

    manage changes

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,1 +1,34 @@
 import Card from "@/components/common/Card";
+import React from "react";
+import UserCard from "@/components/common/UserCard";
+import { UserData } from "@/interfaces";
+
+interface UsersPageProps {
+  users: UserData[];
+}
+
+const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
+  return (
+    <div>
+      <h1>Users</h1>
+      <div className="user-list">
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+};
+
+export default UsersPage;
