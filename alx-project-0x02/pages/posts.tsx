5c1c663712ed5df39fa4e488d90ac8f0a10bf1db
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-28 00:20:34 +0200

    More changes

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
 
-const Posts = () => {
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  // Format and limit to first 10 posts
+  const posts: PostProps[] = data.slice(0, 10).map((post: any) => ({
+    title: post.title,
+    content: post.body,
+    userId: post.userId,
+  }));
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+const Posts = ({ posts }: { posts: PostProps[] }) => {
   return (
     <div>
       <Header />
       <h1>Posts Page</h1>
+      {posts.map((post, index) => (
+        <PostCard
+          key={index}
+          title={post.title}
+          content={post.content}
+          userId={post.userId}
+        />
+      ))}
     </div>
   );
 };
