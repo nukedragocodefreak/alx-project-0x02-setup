f347545b5b2c36faee3cb7f02cf3277ea4a3245e
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-28 00:11:51 +0200

    Add posts

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,2 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "@/components/layout/Header";
+
+const Posts = () => {
+  return (
+    <div>
+      <header>
+      <h1>Posts Page</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Posts;
